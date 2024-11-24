290297f8ec1b2c29551a38116c1e5284e59e839e
jsbarton <jordansdbarton@gmail.com>
2024-11-24 11:55:42 -0500

    add navbar sections

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,17 @@ import ProfilePic from "./i/profile_pic.jpg";
 export default function Home() {
   return (
     <div className="flex h-screen">
-      <div className="flex flex-col justify-center w-1/3">
+      <div className="flex flex-col justify-center items-center w-1/3">
         <Image
           alt="my face"
           className="max-w-12 max-h-12 rounded-full"
           src={ProfilePic}
         />
-        <div>NavContent</div>
+        <div>About</div>
+        <div>Projects</div>
       </div>
       <div className="w-1 bg-red-500" />
-      <div className="flex flex-col justify-center w-2/3">Content</div>
+      <div className="flex flex-col justify-center w-2/3 px-8">Content</div>
     </div>
   );
 }
