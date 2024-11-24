3d7ee9e030928493425075967631ff36d34b8e19
jsbarton <jordansdbarton@gmail.com>
2024-11-24 11:49:36 -0500

    setup homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
+import ProfilePic from "./i/profile_pic.jpg";
 
 export default function Home() {
   return (
-    <div className="flex">
-      <div className="w-1/3 h-full">
-        <div>Picture</div>
+    <div className="flex h-screen">
+      <div className="flex flex-col justify-center w-1/3">
+        <Image
+          alt="my face"
+          className="max-w-12 max-h-12 rounded-full"
+          src={ProfilePic}
+        />
         <div>NavContent</div>
       </div>
-      <div className="w-2/3 h-full">Content</div>
+      <div className="w-1 bg-red-500" />
+      <div className="flex flex-col justify-center w-2/3">Content</div>
     </div>
   );
 }
