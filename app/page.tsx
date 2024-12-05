07be522826d00bd34b58885272f59ea162a4910d
jsbarton <jordansdbarton@gmail.com>
2024-12-04 21:57:16 -0500

    adjust page layout and add about

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import ProfilePic from "./i/profile_pic.jpg";
 import { MutableRefObject, useRef } from "react";
 
 export default function Home() {
-  const contentWrapper = "bg-red-500 w-50 h-screen m-10";
+  const contentWrapper = "m-20 w-50 h-screen m-10";
   const aboutRef = useRef(null);
   const projectsRef = useRef(null);
 
@@ -17,9 +17,10 @@ export default function Home() {
       });
     }
   };
+
   return (
-    <div className="flex">
-      <div className="flex flex-col h-screen items-center justify-center w-1/3">
+    <>
+      <div className="fixed flex flex-col h-screen items-center justify-center w-1/3">
         <div className="pb-3">Jordan Barton</div>
         <Image
           alt="my face"
@@ -33,18 +34,32 @@ export default function Home() {
           Projects
         </div>
       </div>
-      <div className="w-0.5 bg-red-500" />
-      <div className="flex flex-col justify-around overflow-y-auto w-2/3 px-8">
-        <div className={contentWrapper} ref={aboutRef}>
-          About
+      <div className="flex justify-end">
+        <div className="w-0.5" />
+        <div className="flex flex-col justify-around overflow-y-auto px-8 w-2/3">
+          <div className={contentWrapper} ref={aboutRef}>
+            <p>About</p>
+            <p>
+              My name is Jordan Barton and I am a Software Developer from Prince
+              George's County Maryland
+            </p>
+            <p>
+              When I'm not coding, I enjoy sewing, trying new recipes, shopping,
+              and traveling
+            </p>
+            <p>
+              I've compiled some of my work here, and would love to connect with
+              you!
+            </p>
+          </div>
+          <div className={contentWrapper} ref={projectsRef}>
+            Project 1
+          </div>
+          <div className={contentWrapper}>Project 2</div>
+          <div className={contentWrapper}>Project 3</div>
+          <div className={contentWrapper}>AI playlist generator</div>
         </div>
-        <div className={contentWrapper} ref={projectsRef}>
-          Project 1
-        </div>
-        <div className={contentWrapper}>Project 2</div>
-        <div className={contentWrapper}>Project 3</div>
-        <div className={contentWrapper}>Project 4</div>
       </div>
-    </div>
+    </>
   );
 }
