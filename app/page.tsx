39960f9d49bdbb0d793bb0d5e3d2099fff365fa2
Jordan Barton <jordanbarton@Jordans-MacBook-Pro.local>
2025-03-18 16:30:07 -0400

    some refactor, starting multi-line welcome implementation

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,15 @@ import ProfilePic from "./i/profile-pic.jpg";
 import GitHubLogo from "./i/github-mark-white.svg";
 import LinkedInLogo from "./i/linked-in-logo.png";
 import { MutableRefObject, useEffect, useRef, useState } from "react";
+import { contentWrapper, title } from "./styles/styles";
 
 export default function Home() {
-  const aboutRef = useRef(null);
   const projectsRef = useRef(null);
+  const aboutRef = useRef(null);
   const [currentWelcomeText, setCurrentWelcomeText] = useState<string>("");
+  // Hi, I'm Jordan.
+  // Current Position: Fullstack Engineer II @ Quizlet
+  // Location: New York, NY
  
   const handleScroll = (ref: MutableRefObject<any>) => {
     if (ref.current) {
@@ -19,23 +23,25 @@ export default function Home() {
     }
   };
   
-  let builder = "";
+  // TODO: Break out into its own component
+  let firstLineBuilder = "";
+  let secondLineBuilder = "";
+  let thirdLineBuilder = "";
   let i = 0;
-  const finalWelcomeText = "Hi, I'm Jordan. I'm a Software Engineer.";
+  const firstLineWelcomeText = "Hi, I'm Jordan.";
+  const secondLineWelcomeText = "Current Position: Fullstack Engineer II @ Quizlet";
+  const thirdLineWelcomeText = "Location: New York, NY";
   useEffect(() => {
     setInterval(() => {
-      if (i < finalWelcomeText.length) {
-        builder+= finalWelcomeText.charAt(i);
-        setCurrentWelcomeText(builder);
+      if (i < firstLineWelcomeText.length) {
+        firstLineBuilder+= firstLineWelcomeText.charAt(i);
+        setCurrentWelcomeText(firstLineBuilder);
         i++;
       }
     }, 100);
   }, []);
+  // END TODO
   
-  // move these to another file
-  const contentWrapper = "w-50 h-screen my-10";
-  const title = "text-3xl pb-6 mt-8";
-
   return (
     <>
       <div className="flex flex-col">
@@ -52,11 +58,11 @@ export default function Home() {
             className="max-w-[100px] max-h-[100px] rounded-full"
             src={ProfilePic}
           />
-          <div className="pt-4" onClick={() => handleScroll(aboutRef)}>
-            About
-          </div>
           <div className="pt-2" onClick={() => handleScroll(projectsRef)}>
             Projects
+          </div>
+          <div className="pt-4" onClick={() => handleScroll(aboutRef)}>
+            About
           </div>
           <div className="flex h-5 w-5 my-5">
             <Image alt="github-logo" src={GitHubLogo} />
@@ -65,21 +71,6 @@ export default function Home() {
         </div>
         <div className="flex">
           <div className="flex flex-col justify-around overflow-y-auto px-8">
-            <div className={contentWrapper} ref={aboutRef}>
-              <p className={title}>About</p>
-              <p>
-                My name is Jordan Barton and I am a Software Developer from
-                Prince George's County Maryland
-              </p>
-              <p>
-                When I'm not coding, I enjoy sewing, trying new recipes,
-                shopping, and traveling
-              </p>
-              <p>
-                I've compiled some of my work here, and would love to connect
-                with you!
-              </p>
-            </div>
             <div className={contentWrapper} ref={projectsRef}>
               <p className={title}>Project 1</p>
               <p>Here's information about project 1</p>
@@ -88,11 +79,28 @@ export default function Home() {
 
             <div className={contentWrapper}>
               <p className={title}>Project 2</p>
-              <p>Here's information about project 1</p>
+              <p>Here's information about project 2</p>
               <p>Maybe a demo will be here too</p>
             </div>
           </div>
         </div>
+        <div className={contentWrapper} ref={aboutRef}>
+              <p className={title}>About</p>
+              {/* Move messages to another file */} 
+              <p>
+                I'm Jordan, I grew up in Prince George's County, Maryland and
+                attended the University of Maryland, College Park where I received
+                my Bachelor's in Computer Science.
+              </p>
+              <p>
+                When I'm not coding, I enjoy sewing, trying new recipes,
+                shopping, and traveling.
+              </p>
+              <p>
+                I've compiled some of my work here. If you'd like to collaborate on 
+                a project, chat about my work, or just say hi, please feel free to reach out!
+              </p>
+            </div>
       </div>
     </>
   );
---
+++ app/styles/styles.ts
@@ -0,0 +1,2 @@
+export const contentWrapper = "w-50 h-screen my-10";
+export const title = "text-3xl pb-6 mt-8";
