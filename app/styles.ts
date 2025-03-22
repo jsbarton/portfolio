f88dc6119c2f33420ed6cc3c73f87295f169414a
jsbarton <jordansdbarton@gmail.com>
2025-03-22 14:56:24 -0400

    update project title styling

--- app/styles.ts
+++ app/styles.ts
@@ -21,5 +21,5 @@ export const blinkCaretAnimation = (lineVersion: string) => `inline-block h-5 mb
  */
 
 export const contentWrapper = 'h-screen my-10 w-50';
-export const title = 'mt-8 pb-6 text-3xl';
+export const title = 'mt-8 ml-[16px] pb-6 text-3xl';
 export const gridBlock = 'bg-gray-100 hover:bg-gray-300 h-[200px] p-4 rounded transition duration-300 ease-in';
